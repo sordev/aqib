2da6f1cd50873aa561650d0e41352afca6ee22ce
Ochir Sanjaadorj <sor.net@gmail.com>
2017-02-25 21:33:57 +0800

    update

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -9,6 +9,7 @@
 #define CLK 2
 Nokia_5110 lcd = Nokia_5110(RST, CE, DC, DIN, CLK);
 
+// https://www.dfrobot.com/wiki/index.php/PM2.5_laser_dust_sensor_SKU:SEN0177
 //******************************
 //*Abstract: Read value of PM1,PM2.5 and PM10 of air quality
 //
@@ -21,17 +22,17 @@ Nokia_5110 lcd = Nokia_5110(RST, CE, DC, DIN, CLK);
 #define LENG 31   //0x42 + 31 bytes equal to 32 bytes
 unsigned char buf[LENG];
 
-int PM01Value = 0;        //define PM1.0 value of the air detector module
-int PM2_5Value = 0;       //define PM2.5 value of the air detector module
+int PM1Value = 0;        //define PM1.0 value of the air detector module
+int PM25Value = 0;       //define PM2.5 value of the air detector module
 int PM10Value = 0;       //define PM10 value of the air detector module
-
 
 #include <ESP8266WiFi.h>
 const char ssid[] = "devsor";
 const char pass[] = "97070767";
+const char api_url[] = "api.utaa.mn";
+const char api_key[] = "3aa18dbe7df6844974e199e327c5c30e";
+const char unwiredlabs_key[] = "918092cf2502c3";
 int tries = 0;
-
-unsigned long starttime;
 
 // Include DHT LIbrary
 // DHT Library from https://github.com/adafruit/DHT-sensor-library required https://github.com/adafruit/Adafruit_Sensor
@@ -45,33 +46,17 @@ float temp = 0;
 
 void setup(void)
 {
+  Serial.begin('9600');
+  Serial.setTimeout(1500);    //set the Timeout to 1500ms, longer than the data transmission periodic time of the sensor
+  setupSensor();
   ESP.wdtDisable(); // Disable Watchdog
   ESP.wdtEnable(WDTO_8S); // Enabling Watchdog
-  connectWiFi();
-  starttime = millis(); //Fetching the current time
-
-  //  lcd.setContrast(90);
-  lcd.print("Please Wait ...");
-  delay(1000);
-  lcd.clear();
-
-  lcd.print("Hi there");
-  lcd.println(":D");
-
-  lcd.setCursor(0, 5);
-  lcd.println("1 2 3 ...");
-
-  dht.begin();
-  Serial.begin(9600);
-  Serial.setTimeout(1500);    //set the Timeout to 1500ms, longer than the data transmission periodic time of the sensor
 }
 
 void loop(void)
 {
   humidity = dht.readHumidity();
   temp = dht.readTemperature();
-  Serial.println("Humi: " + (String)humidity + "%");
-  Serial.println("Temp: " + (String)temp + "C");
   lcd.clear();
   lcd.println("Humi: " + (String)humidity + "%");
   lcd.println("Temp: " + (String)temp + "C");
@@ -79,75 +64,86 @@ void loop(void)
 
   if (Serial.find(0x42)) {  //start to read when detect 0x42
     Serial.readBytes(buf, LENG);
-
     if (buf[0] == 0x4d) {
       if (checkValue(buf, LENG)) {
-        PM01Value = transmitPM01(buf); //count PM1.0 value of the air detector module
-        PM2_5Value = transmitPM2_5(buf); //count PM2.5 value of the air detector module
+        PM1Value = transmitPM1(buf); //count PM1.0 value of the air detector module
+        PM25Value = transmitPM25(buf); //count PM2.5 value of the air detector module
         PM10Value = transmitPM10(buf); //count PM10 value of the air detector module
       }
     }
   }
 
-  static unsigned long OledTimer = millis();
-  if (millis() - OledTimer >= 1000)
-  {
-    OledTimer = millis();
-
-    Serial.print("PM1.0: ");
-    Serial.print(PM01Value);
-    Serial.println("  ug/m3");
-
-    Serial.print("PM2.5: ");
-    Serial.print(PM2_5Value);
-    Serial.println("  ug/m3");
-
-    Serial.print("PM1 0: ");
-    Serial.print(PM10Value);
-    Serial.println("  ug/m3");
-    Serial.println();
-  }
   lcd.clear();
   lcd.println("PM in ug/m3");
   lcd.println(" ");
-  lcd.println("PM 1.0: " + (String)PM01Value);
-  lcd.println("PM 2.5: " + (String)PM2_5Value);
+  lcd.println("PM 1.0: " + (String)PM1Value);
+  lcd.println("PM 2.5: " + (String)PM25Value);
   lcd.println("PM 10: " + (String)PM10Value);
   delay(3000);
 
   ESP.wdtFeed(); // Reset the WatchDog
 }
 
-void connectWiFi() {
-  lcd.clear();
-  lcd.println("Connecting WiFi:");
-  lcd.println((String)ssid);
+boolean connectWiFi() {
   if (WiFi.status() == WL_CONNECTED) {
     lcd.clear();
     lcd.println("Connected WiFi:");
     lcd.println((String)ssid);
+    delay(3000);
+    return true;
+  } else {
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED || tries  < 4) {
+      tries++;
+      lcd.println("Connecting:");
+      lcd.println((String)ssid);
+      lcd.println("Tries:" + String(tries));
+      delay(1000);
+    }
+    lcd.println("Can't connect:");
+    lcd.println((String)ssid);
+    delay(2000);
+    return false;
+  }
+}
+
+void setupSensor() {
+  lcd.print("Booting Up ...");
+  delay(3000);
+  connectWiFi();
+  // updateThingSpeak("1=" + String(concentration[PM10], DEC) + "&2=" + String(count[PM10], DEC) + "&3=" + String(concentration[PM25], DEC) + "&4=" + String(count[PM25], DEC));
+}
+
+void getIP() {
+  // https://api.ipify.org?format=json
+}
+
+void getLocation() {
+  // https://unwiredlabs.com/api#documentation
+  // https://ap1.unwiredlabs.com/v2/process.php
+}
+
+void updateApi(String data) {
+  WiFiClient client;
+  if (!client.connect(api_url, 80)) {
     return;
   }
-  tries++;
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    lcd.clear();
-    lcd.println("Connection failed:");
-    lcd.println("Trying:" + tries);
-  }
+  client.print(F("POST /api/update?key="));
+  client.print(api_key);
+  client.print(F("&"));
+  client.print(data);
+  client.print(F(" HTTP/1.1\r\nHost: \r\n\r\n"));
+  client.println();
 }
 
 char checkValue(unsigned char *thebuf, char leng)
 {
   char receiveflag = 0;
   int receiveSum = 0;
-
   for (int i = 0; i < (leng - 2); i++) {
     receiveSum = receiveSum + thebuf[i];
   }
   receiveSum = receiveSum + 0x42;
-
   if (receiveSum == ((thebuf[leng - 2] << 8) + thebuf[leng - 1])) //check the serial data
   {
     receiveSum = 0;
@@ -156,19 +152,19 @@ char checkValue(unsigned char *thebuf, char leng)
   return receiveflag;
 }
 
-int transmitPM01(unsigned char *thebuf)
+int transmitPM1(unsigned char *thebuf)
 {
-  int PM01Val;
-  PM01Val = ((thebuf[3] << 8) + thebuf[4]); //count PM1.0 value of the air detector module
-  return PM01Val;
+  int PM1Val;
+  PM1Val = ((thebuf[3] << 8) + thebuf[4]); //count PM1.0 value of the air detector module
+  return PM1Val;
 }
 
 //transmit PM Value to PC
-int transmitPM2_5(unsigned char *thebuf)
+int transmitPM25(unsigned char *thebuf)
 {
-  int PM2_5Val;
-  PM2_5Val = ((thebuf[5] << 8) + thebuf[6]); //count PM2.5 value of the air detector module
-  return PM2_5Val;
+  int PM25Val;
+  PM25Val = ((thebuf[5] << 8) + thebuf[6]); //count PM2.5 value of the air detector module
+  return PM25Val;
 }
 
 //transmit PM Value to PC
